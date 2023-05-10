04ea7fb69d59e311098b0f3abd99ad933ad79855
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2023-05-10 18:54:27 -0400

    Add project files.

---
+++ ori-save-mover/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System.Diagnostics;
+
+namespace ori_save_mover
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        const string DefaultLabelText = "Choose file to shift";
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private OpenFileDialog openFileDialog1;
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.readOnlyPathTextBox = new System.Windows.Forms.TextBox();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.shiftDownwardsButton = new System.Windows.Forms.Button();
+            this.shiftUpwardsButton = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.readOnlyPathTextBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chooseFileButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.shiftDownwardsButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.shiftUpwardsButton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.infoLabel, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(8);
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 111);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // readOnlyPathTextBox
+            // 
+            this.readOnlyPathTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.readOnlyPathTextBox.HideSelection = false;
+            this.readOnlyPathTextBox.Location = new System.Drawing.Point(11, 11);
+            this.readOnlyPathTextBox.Name = "readOnlyPathTextBox";
+            this.readOnlyPathTextBox.ReadOnly = true;
+            this.readOnlyPathTextBox.Size = new System.Drawing.Size(262, 23);
+            this.readOnlyPathTextBox.TabIndex = 0;
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chooseFileButton.Location = new System.Drawing.Point(11, 30);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(262, 13);
+            this.chooseFileButton.TabIndex = 1;
+            this.chooseFileButton.Text = "Choose File";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFile_Click);
+            // 
+            // shiftDownwardsButton
+            // 
+            this.shiftDownwardsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.shiftDownwardsButton.Location = new System.Drawing.Point(11, 49);
+            this.shiftDownwardsButton.Name = "shiftDownwardsButton";
+            this.shiftDownwardsButton.Size = new System.Drawing.Size(262, 13);
+            this.shiftDownwardsButton.TabIndex = 2;
+            this.shiftDownwardsButton.Text = "Shift Downwards";
+            this.shiftDownwardsButton.UseVisualStyleBackColor = true;
+            this.shiftDownwardsButton.Click += new System.EventHandler(this.shiftDownwardsButton_Click);
+            // 
+            // shiftUpwardsButton
+            // 
+            this.shiftUpwardsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.shiftUpwardsButton.Location = new System.Drawing.Point(11, 68);
+            this.shiftUpwardsButton.Name = "shiftUpwardsButton";
+            this.shiftUpwardsButton.Size = new System.Drawing.Size(262, 13);
+            this.shiftUpwardsButton.TabIndex = 4;
+            this.shiftUpwardsButton.Text = "Shift Upwards";
+            this.shiftUpwardsButton.UseVisualStyleBackColor = true;
+            this.shiftUpwardsButton.Click += new System.EventHandler(this.shiftUpwardsButton_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.infoLabel.Location = new System.Drawing.Point(11, 84);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(262, 19);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.infoLabel.Click += new System.EventHandler(this.infoLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(300, 100);
+            this.Name = "Form1";
+            this.Text = "Ori Save Shifter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox readOnlyPathTextBox;
+        private Button chooseFileButton;
+        private Button shiftDownwardsButton;
+        private Label infoLabel;
+        private Button shiftUpwardsButton;
+    }
+}
---
+++ ori-save-mover/Form1.cs
@@ -0,0 +1,171 @@
+using System.Security;
+using System.Text.RegularExpressions;
+
+namespace ori_save_mover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AutoSize = true;
+            infoLabel.Text = DefaultLabelText;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            shiftUpwardsButton.Enabled = false;
+            shiftDownwardsButton.Enabled = false;
+        }
+
+        private readonly Regex rg = new(@"\A[0-9]+\b");
+        private string[] fileNames;
+        private string path = "";
+        private string fileName = "";
+        private string directory = "";
+        private int start = -1;
+
+        private void resetAllFields()
+        {
+            this.path = "";
+            this.fileName = "";
+            this.directory = "";
+            this.start = -1;
+            this.infoLabel.Text = DefaultLabelText;
+            this.shiftUpwardsButton.Enabled = false;
+            this.shiftDownwardsButton.Enabled = false;
+            this.readOnlyPathTextBox.Text = "";
+        }
+
+        private void chooseFile_Click(object sender, EventArgs e)
+        {
+            resetAllFields();
+
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.path = openFileDialog1.FileName;
+
+                    FileInfo info = new(path);
+                    if (info.DirectoryName is null)
+                    {
+                        // This should never happen, but just in case....
+                        resetAllFields();
+                        this.infoLabel.Text = "Error finding directory name";
+                        return;
+                    }
+
+                    this.fileName = info.Name;
+                    this.directory = info.DirectoryName;
+                    this.fileNames = Directory.GetFiles(directory, "*.uberstate").Select(path => new FileInfo(path).Name).ToArray();
+
+                    if (!this.fileNames.Any())
+                    {
+                        resetAllFields();
+                        this.infoLabel.Text = "Could not find any uberstate files here";
+                        return;
+                    }
+
+                    // Scan directory and make sure ALL files match the pattern properly
+                    foreach (string fileName in fileNames)
+                    {
+                        Match match = rg.Match(fileName);
+                        if (!match.Success)
+                        {
+                            resetAllFields();
+                            this.infoLabel.Text = "ALL uberstate files must match pattern '### - Name'";
+                            return;
+                        }
+                    }
+
+                    this.infoLabel.Text = "Press a shift button :)";
+
+                    // Find start index
+                    for (int i = 0; i < fileNames.Length; i++)
+                        if (fileNames[i].Equals(fileName))
+                            this.start = i;
+
+                    if (this.start == -1)
+                    {
+                        resetAllFields();
+                        this.infoLabel.Text = $"Could not find start index";
+                        return;
+                    }
+
+                    this.shiftUpwardsButton.Enabled = true;
+                    this.shiftDownwardsButton.Enabled = true;
+                    this.readOnlyPathTextBox.Text = path;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void shiftDownwardsButton_Click(object sender, EventArgs e)
+        {
+            Shift(-1);
+        }
+
+        private void shiftUpwardsButton_Click(object sender, EventArgs e)
+        {
+            Shift(1);
+        }
+
+        public void Shift(int direction)
+        {
+            // Map full path to just file names
+            string[] fileNamesNew = new string[fileNames.Length];
+
+            if (direction == -1 && start == 0 && int.Parse(rg.Match(fileNames[0]).Value) == 0)
+            {
+                resetAllFields();
+                this.infoLabel.Text = $"Cannot downshift starting 0";
+                return;
+            }
+
+            // We know matches for sure exist here
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                string currFileName = fileNames[i];
+                fileNamesNew[i] = currFileName;
+
+                Match match = rg.Match(currFileName);
+
+                string oldPrefix = match.Value;
+                int prefixAsInt = int.Parse(oldPrefix);
+
+                if (i >= this.start)
+                {
+                    // Create a formatter that follows the same format they did
+                    // (e.g: if they did "123" it will be "000" format, if they did "12" it will be "00" format)
+                    string format = new('0', oldPrefix.Length);
+
+                    // Change the prefix to be the old one, but up or down shifted by 1, then formatted the same way they formatted it
+                    string newPrefix = (prefixAsInt + direction).ToString(format);
+
+                    // Replace the 
+                    string newFileName = currFileName.Replace(oldPrefix, newPrefix);
+
+                    string oldFullPath = $"{directory}\\{currFileName}";
+                    string newFullPath = $"{directory}\\{newFileName}";
+
+                    File.Move(oldFullPath, newFullPath);
+                }
+            }
+
+            resetAllFields();
+            this.infoLabel.Text = $"Done! Another one?";
+
+        }
+
+        private void infoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
